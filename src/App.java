fc47f8eed402211031ebadb1fc4ce06ad0767083
medinahj21 <medinaho21@gmail.com>
2021-07-22 19:58:44 -0500

    Java Lessons

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
+
+    private String nombre;
+    private int identificacion;
+    private double estatura;
+    private float peso;
+    private boolean estaVacunado;
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+         {System.out.println("Aqui aprendiendo Java");
+            
+        int resultado = 45 + 12;}
+    
+    //operadores logicos
+    resultado= 5 > 8 && 4 <= 2;
+    
+
     }
 }
